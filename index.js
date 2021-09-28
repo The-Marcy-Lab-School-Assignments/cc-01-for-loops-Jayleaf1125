0fe4f11cb82fe997241af2c7aff710c7aa800b0d
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-09-28 14:23:09 +0000

    Jason's Code Challenge 01

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 //1
 function countToFifty(){
-  
+  for(let i=1; i < 51; i++) {
+      console.log(i);
+  }
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(number){
+  for(let i=1; i <= number; i++) {
+      console.log(i);
+  }
 }
 
 //3
 function threeAndFiveInFifty(){
-  
+    for(let i=1; i <= 50; i++) {
+      if(i % 3 === 0 || i % 5 === 0) {
+          console.log(i);
+      }
+  }
 }
 
 //4
-function threeAndFive(){
-  
+function threeAndFive(number){
+  for(let i=1; i <= number; i++) {
+      if(i % 3 === 0 || i % 5 === 0) {
+          console.log(i);
+      }
+  }
 }
 
+// Call
+countToFifty();
+countFromOne(5);
+threeAndFiveInFifty();
+threeAndFive(20);
 
